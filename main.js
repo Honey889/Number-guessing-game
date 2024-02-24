bc241d6aaf3aebb39aee0fdb21fe6c09904bba1d
Honey889 <156218686+Honey889@users.noreply.github.com>
2024-02-24 16:31:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// import inquirer from "inquirer"
+// const systemGeneratedNo = Math.floor(Math.random() * 10)
+// console.log(systemGeneratedNo)
+// import inquirer from "inquirer";
+// type ansType = {
+//     userGuess : number
+// }
+// const systemGeneratedNo = Math.floor(Math.random() * 10)
+// const answers: ansType = await inquirer.prompt([
+//     {
+//         type: "number",
+//         name : "userGuess",
+//         message: "Enter your guess number from 1 to 10"
+//     }
+// ])
+// console.log(answers.userGuess)
+import inquirer from "inquirer";
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Enter your guess number from 1 to 10"
+    }
+]);
+const { userGuess } = answers;
+console.log(userGuess, "userGuess", systemGeneratedNo, 'systemNo.');
+if (userGuess == systemGeneratedNo) {
+    console.log("Yay! your guess is correct \n You won");
+}
+else {
+    console.log("please try again , Better luck next time");
+}
